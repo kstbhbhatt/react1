0da1c75c5ec14464e7ada9b9cad0600c6110657c
Bhatt <kaubhatt@deloitte.com>
2023-05-29 20:22:13 +0530

    animate

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import Aos from "aos";
+import 'aos/dist/aos.css'
 
 function Card(props){
+    React.useEffect(()=> {
+        Aos.init({duration:1000})
+    })
     return(
         <div className="card">
             <img src={props.url}/>
             <h3>{props.title}</h3>
-            <p>{props.descp}</p>
+            <p  data-aos="fade-up">{props.descp}</p>
             <a href="https://www.samsung.com/in/smartphones/galaxy-s23-ultra/" target="_blank">Read More</a>
 
         </div>
--- src/Description.jsx
+++ src/Description.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import Aos from "aos";
+import 'aos/dist/aos.css'
 
 function Description(){
+    React.useEffect(()=> {
+        Aos.init({duration:1000})
+    })
 return(
     <div className="description">
-        <p>Samsung's Galaxy S23 Ultra doesn't dare rock the boat. But what it lacks in excitement, it more than makes up for in what may be the best Android experience on the market right now.</p>
-        <p>One UI is as feature-packed as ever, and the 200MP camera can capture detail unlike any of the competition.</p>
-        <p>It's not perfect, but the Galaxy S23 Ultra is Samsung's best phone yet.</p>
+        <p data-aos="fade-up">Samsung's Galaxy S23 Ultra doesn't dare rock the boat. But what it lacks in excitement, it more than makes up for in what may be the best Android experience on the market right now.</p>
+        <p data-aos="fade-up">One UI is as feature-packed as ever, and the 200MP camera can capture detail unlike any of the competition.</p>
+        <p data-aos="fade-up">It's not perfect, but the Galaxy S23 Ultra is Samsung's best phone yet.</p>
     </div>
 )
 }
