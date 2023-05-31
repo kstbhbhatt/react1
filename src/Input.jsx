28e9c892ec16ea853f8c3da9664b17a98095fc8b
Bhatt <kaubhatt@deloitte.com>
2023-05-31 16:25:45 +0530

    formschange

--- src/Input.jsx
+++ src/Input.jsx
@@ -4,7 +4,7 @@ function Input(prop){
 
 
     return(
-        <input onChange={validate} name={prop.name} type={prop.type} placeholder={prop.name} />
+        <input name={prop.name} type={prop.type} placeholder={prop.name} />
     )
 }
 export default Input 
