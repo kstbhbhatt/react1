61189ac43aa419d7b96ead66c20d9338884529ba
Bhatt <kaubhatt@deloitte.com>
2023-05-31 16:36:25 +0530

    design change

--- src/Form.jsx
+++ src/Form.jsx
@@ -3,7 +3,12 @@ import Input from './Input';
 
 function Form(props){
     function showMessage(e){
-        alert('WE are please that you are interested, will soon add this functionality');
+        if( e.target.closest('form').getAttribute('id') == 'signup-form'){
+            alert('Sign Up Fucntionality will be enabled soon!');
+        }else{
+            alert('We are please that you are interested, will soon add this functionality!');
+        }
+        
         e.target.closest('form').previousSibling.previousSibling.click()
         // document.querySelector('.close-pop-up').click()
         e.preventDefault();
@@ -11,7 +16,7 @@ function Form(props){
     return(
         <div className='form-div' >
             <div onClick={props.close} id={props.id + "-close"} className="close-pop-up">X</div>
-            <h3>{props.heading}</h3>
+            <h2>{props.heading}</h2>
             <form action="" id={props.id} >
                {props.inputs.map((value, index)=>(<Input key={index} name={value.name} type={value.type}/>))}
                {props.textbox&&<textarea name="" id="" lines='3'></textarea>} 
--- src/Header.jsx
+++ src/Header.jsx
@@ -23,7 +23,7 @@ function Header(){
     return(
         <header>
             <nav>
-                <h3>Samsung Galaxy S23</h3>
+                <h3><a href='#'>Samsung Galaxy S23</a></h3>
                 <button onClick={toggleNav} className ={ham ?"hamburg show d-none": "hamburg d-none"}><div className="hamburger"></div></button>
                 <div style={ham?({top:"70px"}):({top:"-500px"})} className="lists">
                 <ul className="navbar-list">
